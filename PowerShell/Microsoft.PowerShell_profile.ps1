1fa1fa333421b15935ce6b6dddc949359c29d466
Manmeet Gill <contact@manmeetgill.com>
2016-11-21 19:08:39 +1100

    Add Powershell profile

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿function MP4automater { py -2 C:\MP4Automater\manual.py $args }
+function PDFClean { C:\Users\Manu\Desktop\Scripts\mupdf\mutool.exe clean $args }
+function Refresh-Path { $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") }
+New-Alias activate ./VirtualEnv/Scripts/activate.ps1
+function which([string]$cmd) {gcm -ErrorAction "SilentlyContinue" $cmd | ft Definition}function which([string]$cmd) {gcm -ErrorAction "SilentlyContinue" $cmd | ft Definition}
+chcp 65001
